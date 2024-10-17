807819d40899e07a672a7d9b0fa0a1c3f7f0ecc1
Нармаматов <sadirbekovich04@gmail.com>
2024-10-17 19:31:05 +0600

    Add files via upload

---
+++ main.js
@@ -0,0 +1,149 @@
+let dO = document.querySelector(".do");
+window.addEventListener("keydown", (e) => {
+  if (e.key === "z") {
+    let nDo = new Audio();
+    nDo.src = "./sounds/do.mp3";
+    nDo.play();
+    dO.style.borderBottom = "15px solid rgb(62, 61, 61)";
+    dO.style.borderRight = "5px solid rgb(62, 61, 61)";
+    dO.style.background = "linear-gradient(to top, #616060, #ffffff)";
+
+    setTimeout(() => {
+      dO.style.background = "white";
+      dO.style.borderBottom = "25px solid rgb(62, 61, 61)";
+      dO.style.borderRight = "15px solid rgb(62, 61, 61)";
+      dO.style.background = "linear-gradient(to top, #d1d1d1, #ffffff)";
+    }, 100);
+  }
+});
+let rE = document.querySelector(".re");
+window.addEventListener("keydown", (e) => {
+  if (e.key === "x") {
+    let nRe = new Audio();
+    nRe.src = "./sounds/re.mp3";
+    nRe.play();
+    rE.style.borderBottom = "15px solid rgb(62, 61, 61)";
+    rE.style.borderRight = "5px solid rgb(62, 61, 61)";
+    rE.style.background = "linear-gradient(to top, #616060, #ffffff)";
+
+    setTimeout(() => {
+      rE.style.background = "white";
+      rE.style.borderBottom = "25px solid rgb(62, 61, 61)";
+      rE.style.borderRight = "15px solid rgb(62, 61, 61)";
+      rE.style.background = "linear-gradient(to top, #d1d1d1, #ffffff)";
+    }, 100);
+  }
+});
+
+let mI = document.querySelector(".mi");
+window.addEventListener("keydown", (e) => {
+  if (e.key === "c") {
+    let nMi = new Audio();
+    nMi.src = "./sounds/mi.mp3";
+    nMi.play();
+    mI.style.borderBottom = "15px solid rgb(62, 61, 61)";
+    mI.style.borderRight = "5px solid rgb(62, 61, 61)";
+    mI.style.background = "linear-gradient(to top, #616060, #ffffff)";
+
+    setTimeout(() => {
+      mI.style.background = "white";
+      mI.style.borderBottom = "25px solid rgb(62, 61, 61)";
+      mI.style.borderRight = "15px solid rgb(62, 61, 61)";
+      mI.style.background = "linear-gradient(to top, #d1d1d1, #ffffff)";
+    }, 100);
+  }
+});
+
+let fA = document.querySelector(".fa");
+window.addEventListener("keydown", (e) => {
+  if (e.key === "v") {
+    let nFa = new Audio();
+    nFa.src = "./sounds/fa.mp3";
+    nFa.play();
+    fA.style.borderBottom = "15px solid rgb(62, 61, 61)";
+    fA.style.borderRight = "5px solid rgb(62, 61, 61)";
+    fA.style.background = "linear-gradient(to top, #616060, #ffffff)";
+
+    setTimeout(() => {
+      fA.style.background = "white";
+      fA.style.borderBottom = "25px solid rgb(62, 61, 61)";
+      fA.style.borderRight = "15px solid rgb(62, 61, 61)";
+      fA.style.background = "linear-gradient(to top, #d1d1d1, #ffffff)";
+    }, 100);
+  }
+});
+
+let sO = document.querySelector(".so");
+window.addEventListener("keydown", (e) => {
+  if (e.key === "b") {
+    let nSo = new Audio();
+    nSo.src = "./sounds/sol.mp3";
+    nSo.play();
+    sO.style.borderBottom = "15px solid rgb(62, 61, 61)";
+    sO.style.borderRight = "5px solid rgb(62, 61, 61)";
+    sO.style.background = "linear-gradient(to top, #616060, #ffffff)";
+
+    setTimeout(() => {
+      sO.style.background = "white";
+      sO.style.borderBottom = "25px solid rgb(62, 61, 61)";
+      sO.style.borderRight = "15px solid rgb(62, 61, 61)";
+      sO.style.background = "linear-gradient(to top, #d1d1d1, #ffffff)";
+    }, 100);
+  }
+});
+
+let lA = document.querySelector(".la");
+window.addEventListener("keydown", (e) => {
+  if (e.key === "n") {
+    let nLa = new Audio();
+    nLa.src = "./sounds/lia.mp3";
+    nLa.play();
+    lA.style.borderBottom = "15px solid rgb(62, 61, 61)";
+    lA.style.borderRight = "5px solid rgb(62, 61, 61)";
+    lA.style.background = "linear-gradient(to top, #616060, #ffffff)";
+
+    setTimeout(() => {
+      lA.style.background = "white";
+      lA.style.borderBottom = "25px solid rgb(62, 61, 61)";
+      lA.style.borderRight = "15px solid rgb(62, 61, 61)";
+      lA.style.background = "linear-gradient(to top, #d1d1d1, #ffffff)";
+    }, 100);
+  }
+});
+
+let sI = document.querySelector(".si");
+window.addEventListener("keydown", (e) => {
+  if (e.key === "m") {
+    let nSi = new Audio();
+    nSi.src = "./sounds/si.mp3";
+    nSi.play();
+    sI.style.borderBottom = "15px solid rgb(62, 61, 61)";
+    sI.style.borderRight = "5px solid rgb(62, 61, 61)";
+    sI.style.background = "linear-gradient(to top, #616060, #ffffff)";
+
+    setTimeout(() => {
+      sI.style.background = "white";
+      sI.style.borderBottom = "25px solid rgb(62, 61, 61)";
+      sI.style.borderRight = "15px solid rgb(62, 61, 61)";
+      sI.style.background = "linear-gradient(to top, #d1d1d1, #ffffff)";
+    }, 100);
+  }
+});
+let do2 = document.querySelector(".do2");
+window.addEventListener("keydown", (e) => {
+  if (e.key === ",") {
+    let nDo2 = new Audio();
+    nDo2.src = "./sounds/do2.mp3";
+    nDo2.play();
+    do2.style.borderBottom = "15px solid rgb(62, 61, 61)";
+    do2.style.borderRight = "5px solid rgb(62, 61, 61)";
+    do2.style.background = "linear-gradient(to top, #616060, #ffffff)";
+
+    setTimeout(() => {
+      do2.style.background = "white";
+      do2.style.borderBottom = "25px solid rgb(62, 61, 61)";
+      do2.style.borderRight = "15px solid rgb(62, 61, 61)";
+      do2.style.background = "linear-gradient(to top, #d1d1d1, #ffffff)";
+    }, 100);
+  }
+});
